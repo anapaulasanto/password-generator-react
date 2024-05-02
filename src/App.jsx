011265538ab1b6bae8540065fea27925c57a7757
Ana paula <149811410+anapaulasanto@users.noreply.github.com>
2024-05-02 17:18:43 -0300

    improves the option to set password size

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import Input from "../components/input";
 function App() {
   const [password, setPassword] = useState("")
   const [copyText, setCopyText] = useState("Copiar")
-  const [passwordSize, setPasswordSize] = useState(12)
-  const [showInput, setShowInpt] = useState(false)
+  const [customSize, setCustomSize] = useState(12)
+  const [showInput, setShowInput] = useState(false)
+
+  const passwordSize = showInput ? customSize : 8
 
 
   function generate() {
-    const characters = "!@#$%¨&*1234567890qwertyuiopasdfghjlçzxcvbnmZXCVBNMQWERTYUIOPASDFGHJKLÇ,.;/"
+    const characters = "!@#$%¨&*1234567890qwertyuiop´[asdfghjklç~]zxcvbnm,.;QWERTYUIOP`{ASDFGHJKLÇ^}ZXCVBNM<>:/*-"
     let newPassword = ""
     for (let i = 0; i < passwordSize; i++) {
       let randomNumber = Math.floor(Math.random() * characters.length)
@@ -30,18 +32,21 @@ function App() {
     <div className="app">
       <h1>Gerador de senha</h1>
       <div>
-        <label htmlFor="showInput">Definir tamanho</label>
+        <label htmlFor="showInput">Definir tamanho da senha:</label>
         <input 
           type="checkbox"
           id="showInput"
           value={showInput}
-          onChange={() => setShowInpt(currentState => !currentState)}
+          onChange={() => setShowInput(currentState => !currentState)}
         />
       </div>
-      <div>
-        <label htmlFor="passwordSize">Tamanho:</label>
-        <Input passwordSize={passwordSize} setPasswordSize={setPasswordSize} />
+      {showInput && (
+        <div>
+          <label htmlFor="passwordSize">Tamanho:</label>
+          <Input passwordSize={customSize} setPasswordSize={setCustomSize} />
       </div>
+      )}
+      
       <button onClick={generate}>Gerar senha de {passwordSize} caracteres</button>
       <button onClick={copyToClipboard}> {copyText} </button>
       <div>{password}</div>
